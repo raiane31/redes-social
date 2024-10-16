33a771fc132b3297d61f60a0d860f32b5f4d169a
raiane31 <105453274+raiane31@users.noreply.github.com>
2024-10-15 21:01:25 -0300

    const

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -1,3 +1,6 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-console.log(url);
+async function visualizarInformacoesGlobais() {
+    const res = fetch(url);
+    
+}
